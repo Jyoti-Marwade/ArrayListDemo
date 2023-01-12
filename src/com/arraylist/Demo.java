aefb631849a9ec024f8ed5140853790f693c35bc
your namejyotimarwade <jyotimarwade220@gmail.com>
2023-01-12 21:53:53 +0530

    files added successfully

---
+++ src/com/arraylist/Demo.java
@@ -0,0 +1,15 @@
+package com.arraylist;
+
+import java.util.ArrayList;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		ArrayList<String> al=new ArrayList<String>();
+		al.add("Jyoti");
+		al.add("Rohit");
+		al.add("Mayur");
+		al.add("Yuvraj");
+		System.out.println(al);
+	}
+}
