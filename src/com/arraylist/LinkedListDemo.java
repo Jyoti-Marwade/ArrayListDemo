9a0ba0093c13f972888f30104a075ef93f86f673
your namejyotimarwade <jyotimarwade220@gmail.com>
2023-01-17 22:12:16 +0530

    Added LinkedList Demo code

---
+++ src/com/arraylist/LinkedListDemo.java
@@ -0,0 +1,15 @@
+package com.arraylist;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListDemo {
+public static void main(String[] args) {
+	List<Integer> list=new LinkedList<Integer>();
+	list.add(10);
+	list.add(20);
+	list.add(30);
+	list.add(40);
+	System.out.println(list);
+}
+}
